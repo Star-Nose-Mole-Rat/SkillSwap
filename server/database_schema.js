ed891067471c33477c62b3a9ad88ea41e4ae6ee7
Elizabeth Sinclair <elizabethsinc.12@gmail.com>
2024-04-11 18:46:12 -0700

    gitignore updated and preliminary schemas defined

---
+++ server/database_schema.js
@@ -0,0 +1,24 @@
+// this file will hold and export the schema for our user Data base
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  displayName: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  skillVideos: [{ type: mongoose.Schema.Types.ObjectId, ref: "SkillVideo" }],
+  points: { type: Number },
+  savedSkills: { type: Array },
+});
+const User = mongoose.model("user", userSchema);
+
+const skillsVideoSchema = new Schema({
+  subject: { type: Number, required: true },
+  title: { type: Number, required: true },
+  url: { type: Number, required: true },
+});
+
+const SkillVideo = mongoose.model("skillVideo", skillsVideoSchema);
+
+module.exports = { User, SkillVideo };
