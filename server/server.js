6cc338330d1ce7c301be51f2795fc792f835d738
Kobe Fung <kobefungs@gmail.com>
2024-04-12 08:51:08 +0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = 8080;
+const apiRouter = require('./routes/api.js');
 
+app.use(express.json());
+
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+//NOTE: catch all route handler for any request to an unknown route
+app.use((req, res) => {
+  this.response.sendStatus(404);
+});
 
-	return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-  });
-	
-app.listen(8080);
+// global error handler
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
+
+module.exports = app;
