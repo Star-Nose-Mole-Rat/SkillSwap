0016a2ae626ed5cc21a90e0441aeffbb38208566
Yoon Joo Woo <yjdream86@gmail.com>
2024-04-12 01:10:10 -0400

    signup page created and register button redirect to main

--- client/containers/Signup.jsx
+++ client/containers/Signup.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const navigate = useNavigate();
+
+    const handleRegister= (e) => {
+        e.preventDefault();
+        navigate('/search');
+    }
 
     return (
         <div>
-
+            <form onSubmit={handleRegister}>
+                <label>Full Name:
+                    <input type='text'/>
+                </label>
+                <br/>
+                <label>E-mail:
+                    <input type='email'/>
+                </label>
+                <br/>
+                <label>Username:
+                    <input type='text'/>
+                </label>
+                <br/>
+                <label>Password:
+                    <input type='password'/>
+                </label>
+                <br/>
+                <button type='submit'>Register</button>
+            </form>
         </div>
     )
 };
