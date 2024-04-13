ff10391c820ce18ff140c676f0faf200a2f6d438
AshrafMKhan <ashrafkhaneetli@gmail.com>
2024-04-13 16:20:14 -0400

    redux 2

--- client/Ashraf_Test_Component.js
+++ client/Ashraf_Test_Component.js
@@ -3,14 +3,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { addUser } from './userSlice';
+import { addSkill } from './userSlice';
 
 const Ashraf_Test_Component = () => {
 	const dispatchAddUser = useDispatch();
 	dispatchAddUser(addUser('ashrafkhaneetli@gmail.com'));
+  const dispatchAddSkill = useDispatch();
+  dispatchAddSkill(addSkill('Electrical Power Calculations'));
+  const skills = useSelector(state => state.users.skills);
   const info = useSelector(state => state.users.username);
   return(
     <div>
 			<p>info:{info}</p>
+      <p>skill index 0:{skills[0]}</p>
+      
     </div>
   );
 };
