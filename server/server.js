a9de63ee9312ffebb85a46317463c405eb8e25be
Elizabeth Sinclair <elizabethsinc.12@gmail.com>
2024-04-15 20:17:20 -0700

    changed server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8080;
+const PORT = 3000;
 
 const userController = require("./controllers/userController.js");
 const profileController = require("./controllers/profileController.js");
