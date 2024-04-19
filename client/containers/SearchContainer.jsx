b90ee800947aa8428f90afafa3d5df70a903498b
Elizabeth Sinclair <elizabethsinc.12@gmail.com>
2024-04-18 19:36:13 -0700

    95 percent there guys!

--- client/containers/SearchContainer.jsx
+++ client/containers/SearchContainer.jsx
@@ -1,44 +1,69 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Button } from 'react-bootstrap';
-import NavBar from '../components/Nav.jsx';
+import React, { useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
+import NavBar from "../components/Nav.jsx";
+import YoutubeEmbed from "../components/YoutubeEmbed.jsx";
+import { addVideos } from "../userSlice.js";
 
 const SearchContainer = () => {
-  const [search, setSearch] = useState('');
-
-  const videos = useSelector(state => state.users.videos);
-
-
+  const [search, setSearch] = useState("");
+  const [videos, setVideos] = useState([]);
+  // const videos = useSelector((state) => state.users.videos);
+  // const dispatch = useDispatch();
   const handleSearchChange = (e) => {
     e.preventDefault();
     setSearch(e.target.value);
-  }
+  };
 
-  const handleSearch = () => {
-    console.log('searching for: ', search);
-    fetch('/searchKeyword?searchword=' + search, {
-        headers: {
-          'Content-Type': 'application/json',
-        }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log("searching for: ", search);
+    fetch("/searchKeyword?searchword=" + search, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        // console.log(data[0].url);
+        // dispatch(addVideos([data[0].url]));
+        setVideos(data);
       })
-      .then(data => data.json())
-      .then(data => console.log(data))
-      .catch(err => { console.log('invalid search request', err)});
-  }
 
-    return (
-        <div>
-            <NavBar />
-            <div className='d-flex justify-content-center' style={{ marginTop: '10px' }}>
-              <input type='text' id='input_search' onChange={handleSearchChange} placeholder='Search...' style={{ marginRight: '10px' }}/>
-                <Button onClick={handleSearch} className='btn btn-info'>Search</Button>
-            </div>
-            <div className='searchResults'>
-              {/* recent added videos by users should be displayed */}
-              {videos.map(video => <p>{video}</p>)}
-            </div>
-        </div>
-    )
+      // url location: data[0].url
+      // [{url: },]
+      .catch((err) => {
+        console.log("invalid search request", err);
+      });
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <div
+        className="d-flex justify-content-center"
+        style={{ marginTop: "10px" }}
+      >
+        <input
+          type="text"
+          id="input_search"
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          style={{ marginRight: "10px" }}
+        />
+        <Button onClick={handleSearch} className="btn btn-info">
+          Search
+        </Button>
+      </div>
+      <div className="searchResults">
+        {/* recent added videos by users should be displayed */}
+        {videos.map((video, i) => (
+          <YoutubeEmbed key={i} embedId={video.url} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchContainer;
--- client/containers/UserProfile.jsx
+++ client/containers/UserProfile.jsx
@@ -1,72 +1,103 @@
-import React from 'react';
-import { Button, Col, Row, Container } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { addVideo } from '../userSlice';
-import { useState } from 'react';
-import NavBar from '../components/Nav.jsx';
-import YoutubeEmbed from '../components/YoutubeEmbed.jsx';
+import React from "react";
+import { Button, Col, Row, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { addVideo } from "../userSlice";
+import { useState } from "react";
+import NavBar from "../components/Nav.jsx";
+import YoutubeEmbed from "../components/YoutubeEmbed.jsx";
 
 const userProfile = () => {
-    const [video, setVideo] = useState('');
+  const [video, setVideo] = useState("");
+  const [title, setTitle] = useState("");
+  const [subject, setSubject] = useState("");
 
-    const dispatch = useDispatch();
-    const listOfVideos = useSelector(state => state.users.videos);
-    const username = useSelector(state => state.users.username);
-    const points = useSelector(state => state.users.points);
-    const displayName = useSelector(state => state.users.displayName);
-    
+  const dispatch = useDispatch();
+  const listOfVideos = useSelector((state) => state.users.videos);
+  const username = useSelector((state) => state.users.username);
+  const points = useSelector((state) => state.users.points);
+  const displayName = useSelector((state) => state.users.displayName);
+  const userID = useSelector((state) => state.users.userId);
 
-    const handleAddVideo = e => {
-        e.preventDefault();
-        
-        // change to POST request with the body of username, uri, keywords
-        fetch(`/addSkill`, {method: 'POST', headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({user: username, url: video, title: '', subject: '', keyword: ''})})
-        .then(data => {
-            if (data.ok) {
-                dispatch(addVideo(video));
-            }
-        })
-        .catch(err => console.log('Error in handleAddVideo', err))
-    }
-    return (
-        <div>
-          <NavBar />
-          <Row className='justify-content-center'>
-            <div className='text-center'>
-              <h2>User Profile</h2>
-            </div>
-          </Row>
-          <div className='d-flex justify-content-center align-items-center'>
-            <div className='profile-picture'>
-                {/* <img href='#' /> */}
-            </div>
-            <div className='general-info'>
-              <div className='name'>
-                <p>Username: {username}</p>
-                </div>
-                <div>
-                    <p>Display Name: {displayName}</p>
-                </div>
-                <div className='points'>
-                    <p>Points: {points}</p>                    
-                </div>
-                
-                <div className='uploadVideo'>
-                    <p>Upload Video:</p>
-                    <input type = 'text' value={video} onChange={(e) => setVideo(e.target.value)} style={{ marginRight: '10px' }}></input>
-                    <Button className='btn btn-info' onClick={handleAddVideo}>Upload Video</Button>
-                </div>
-                <div className='uploadedVideos'>
-                    <p>Your Uploaded Videos:</p>
-                    <div className='videoDisplay'>
-                    {listOfVideos.map((video, i) => <YoutubeEmbed key={i} embedId={video} />)}
-                    </div>
-                </div>
+  const handleAddVideo = (e) => {
+    e.preventDefault();
+
+    // change to POST request with the body of username, uri, keywords
+    fetch(`/addSkill`, {
+      //localhost:8080/profile/profile
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: userID,
+        url: video,
+        title: title,
+        subject: subject,
+      }),
+    })
+      .then((data) => {
+        if (data.ok) {
+          dispatch(addVideo(video));
+        }
+      })
+      .catch((err) => console.log("Error in handleAddVideo", err));
+  };
+  return (
+    <div>
+      <NavBar />
+      <Row className="justify-content-center">
+        <div className="text-center">
+          <h2>User Profile</h2>
+        </div>
+      </Row>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="profile-picture">{/* <img href='#' /> */}</div>
+        <div className="general-info">
+          <div className="name">
+            <p>Username: {username}</p>
+          </div>
+          <div>
+            <p>Display Name: {displayName}</p>
+          </div>
+          <div className="points">
+            <p>Points: {points}</p>
+          </div>
+
+          <div className="uploadVideo">
+            <p>Upload Video:</p>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+            ></input>
+            <input
+              type="text"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              placeholder="Subject"
+            ></input>
+            <input
+              type="text"
+              value={video}
+              onChange={(e) => setVideo(e.target.value)}
+              style={{ marginRight: "10px" }}
+              placeholder="Enter url"
+            ></input>
+            <Button className="btn btn-info" onClick={handleAddVideo}>
+              Upload Video
+            </Button>
+          </div>
+          <div className="uploadedVideos">
+            <p>Your Uploaded Videos:</p>
+            <div className="videoDisplay">
+              {listOfVideos.map((video, i) => (
+                <YoutubeEmbed key={i} embedId={video} />
+              ))}
             </div>
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default userProfile;
